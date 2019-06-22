52e1e7cc36ee756f18a747bc4744c3805f17cda4
권필성 <pencube1214@gmail.com>
2019-06-23 05:00:53 +0900

    시작

---
+++ ProjectPBR/VirtualObject.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "VirtualObject.h"
+
+
+void VirtualObject::Init()
+{
+}
+
+void VirtualObject::Update()
+{
+}
+
+void VirtualObject::Render()
+{
+}
+
+void VirtualObject::Release()
+{
+}
+
+VirtualObject::VirtualObject()
+{
+}
+
+
+VirtualObject::~VirtualObject()
+{
+}
---
+++ ProjectPBR/VirtualObject.h
@@ -0,0 +1,20 @@
+#pragma once
+class VirtualObject
+{
+protected:
+	virtual void OnInit() = 0;
+	virtual void OnUpdate() = 0;
+	virtual void OnRender() = 0;
+	virtual void OnRelease() = 0;
+
+public:
+
+	virtual void Init();
+	virtual void Update();
+	virtual void Render();
+	virtual void Release();
+
+	VirtualObject();
+	virtual ~VirtualObject();
+};
+
