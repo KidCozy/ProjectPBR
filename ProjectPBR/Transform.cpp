6b77aca493130e8417fd8653f392089a6b8673c3
권필성 <pencube1214@gmail.com>
2019-07-19 01:10:01 +0900

    무시할 폴더, 행렬 추가

--- ProjectPBR/Transform.cpp
+++ ProjectPBR/Transform.cpp
@@ -2,8 +2,36 @@
 
 
 
+void Transform::operator+(XMVECTOR Vec)
+{
+
+	/*** Identity Matrix ***/
+	// 1  0  0  0
+	// 0  1  0  0
+	// 0  0  1  0
+	// 0  0  0  1
+	/***				***/
+	XMMATRIX Mat, Origin;
+
+	Mat = XMMatrixIdentity();
+	Origin = XMLoadFloat4x4(&TRSMatrix);
+
+	Position.x = XMVectorGetX(Vec);
+	Position.y = XMVectorGetY(Vec);
+	Position.z = XMVectorGetZ(Vec);
+
+	Origin *= Mat;
+
+	XMStoreFloat4x4(&TRSMatrix, Origin);
+	
+	return;
+}
+
+
+
 Transform::Transform()
 {
+
 }
 
 
--- ProjectPBR/Transform.h
+++ ProjectPBR/Transform.h
@@ -1,10 +1,76 @@
 #pragma once
-class Transform
+#include<DirectXMath.h>
+using namespace DirectX;
+
+struct Position
 {
 private:
-	
+	XMFLOAT4 Pos;
 public:
-	Transform();
-	~Transform();
+	void operator+(XMVECTOR Vec)
+	{
+		XMFLOAT4 O;
+
+		XMStoreFloat4(&O, Vec);
+
+		Pos.x += O.x;
+		Pos.y += O.y;
+		Pos.z += O.z;
+
+		return;
+	}
+
+	void operator+(XMFLOAT4 Vec)
+	{
+		Pos.x += Vec.x;
+		Pos.y += Vec.y;
+		Pos.z += Vec.z;
+
+		return;
+	}
+
+};
+
+struct Rotation
+{
+private:
+	XMFLOAT4 Rot;
+public:
+	void operator+(XMVECTOR Vec)
+	{
+		XMFLOAT4 O;
+
+		XMStoreFloat4(&O, Vec);
+
+		Rot.x += O.x;
+		Rot.y += O.y;
+		Rot.z += O.z;
+
+		return;
+	}
+
+	void operator+(XMFLOAT4 Vec)
+	{
+		Rot.x += Vec.x;
+		Rot.y += Vec.y;
+		Rot.z += Vec.z;
+
+		return;
+	}
+};
+
+struct Scale
+{
+
+};
+
+struct Transform
+{
+	XMFLOAT4X4 TRSMatrix;
+
+	XMFLOAT4 Position;
+	XMFLOAT4 Rotation;
+	XMFLOAT4 Scale;
+
 };
 
